d6496a37cc2de342c9f0fb65a909e2ac45a9c4de
fatma2705 <arafatma2003@gmail.com>
2024-04-16 10:08:46 +0200

    aggiornamento directory DAO

--- src/main/java/it/prova/gestionearticolijspservletjpamaven/dao/IBaseDAO.java
+++ src/main/java/it/prova/gestionearticolijspservletjpamaven/dao/IBaseDAO.java
@@ -20,7 +20,7 @@ package it.prova.gestionearticolijspservletjpamaven.dao;
 		public void delete(T o) throws Exception;
 
 		// questo mi serve per l'injection
-		public void setEntityManager(EntityManager enti
+		public void setEntityManager(EntityManager entityManager);
 
 	}
 
